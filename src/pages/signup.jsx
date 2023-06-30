b6632edb281d10d29bbb8b74de78b9d30ad91194
Willylaisa <laisawilly7@gmail.com>
2023-06-30 20:17:00 +0300

    More styling to login page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -37,41 +37,41 @@ export default function Login() {
                 <div className='login--section'>
                     <h1>Sign up</h1>
                     <form className="login--form" onSubmit={handleSubmit}>
-                    <div className="login--email label">
-                        <label htmlFor="email">Email: </label>
-                        <input 
-                            type="text" 
-                            name="email" 
-                            id="email" 
-                            placeholder="johndoe@gmail.com"
-                            value={formData.email}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className='label'>
-                        <label htmlFor="password">Password: </label>
-                        <input 
-                            type="password" 
-                            name="password" 
-                            id="password" 
-                            placeholder="********" 
-                            value={formData.password}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className='confirm--password label'>
-                        <label htmlFor="password">Confirm password: </label>
-                        <input 
-                            type="password" 
-                            name="confirmPassword" 
-                            id="confirmPassword" 
-                            placeholder="********" 
-                            value={formData.confirmPassword}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <button>Sign up</button>
-                </form>
+                        <div className="login--email label">
+                            <label htmlFor="email">Email: </label>
+                            <input 
+                                type="text" 
+                                name="email" 
+                                id="email" 
+                                placeholder="johndoe@gmail.com"
+                                value={formData.email}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className='label'>
+                            <label htmlFor="password">Password: </label>
+                            <input 
+                                type="password" 
+                                name="password" 
+                                id="password" 
+                                placeholder="********" 
+                                value={formData.password}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className='confirm--password label'>
+                            <label htmlFor="password">Confirm password: </label>
+                            <input 
+                                type="password" 
+                                name="confirmPassword" 
+                                id="confirmPassword" 
+                                placeholder="********" 
+                                value={formData.confirmPassword}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <button>Sign up</button>
+                    </form>
                 </div>
             </section>
         </div>
