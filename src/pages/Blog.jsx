17b1fb3b9af459a42053325f364f548d2e5efb79
Brian <134489011+Brikita@users.noreply.github.com>
2023-06-30 16:57:46 +0300

    commit 1 Add Blog Component

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
-import "./blog.css"
+import React from 'react';
 
-export default function Blog() {
+
+export default function Blog(props) {
+    /* const newBlog = {
+        id: "",
+        content: "",
+        author: "",
+        coverImg: "",
+        time: "",
+        tags: []
+    } */
+
+    
+    
+
+
+    
     return (
         <div>
-            <h1>Blog Page...</h1>
-            <p>Write your code in this div</p>
+            <div className='blog-container'>
+            
+                <fieldset>
+                    
+                    <legend>Write a new blog</legend>
+                    <textarea type="text" className='blog-input' placeholder='Write your Blog here' name='content' onChange={props.changeContent} value={props.currentBlog.content} />
+
+                    <button className='blog-post-btn' onClick={props.post}> Post Blog </button>
+                </fieldset>
+            </div>
+            <div className="my-posts">
+                <h2>My Posts</h2>
+                <p id='blog-post' ></p>
+            </div>
         </div>
     )
 }
