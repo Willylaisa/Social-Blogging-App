b15aa3cd357ad71d08be502e8f32268fc17ce579
Brian <134489011+Brikita@users.noreply.github.com>
2023-06-30 17:02:42 +0300

    commit 2 add Profile Component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,89 @@
 import React from 'react'
 import "./profile.css"
 
-export default function Profile() {
+import Blog from "./Blog"
+
+
+
+export default function Profile(){
+    const resultDiv = document.getElementById("blog-post")
+    
+    const [blog, setBlog] = React.useState(
+        JSON.parse(localStorage.getItem("blog")) || []
+        );
+    
+
+    /* const [currentBlogId, setCurrentBlogId] = React.useState(
+        (blog[0] && blog[0].id) || ""
+    ) */
+    React.useEffect(() => {
+        //window.addEventListener('load', newBlog())
+        localStorage.setItem("blog", JSON.stringify(blog))
+        
+
+    }, [blog])
+
+    function newBlog() {
+        return ({
+            id: "",
+            content: "",
+            author: "",
+            coverImg: "",
+            time: "",
+            tags: []
+        })
+      
+    }
+    /* setBlog(prevBlog => [blogItem, ...prevBlog])
+      setCurrentBlogId(blogItem.id)  */
+    //const [blogContent, setBlogContent] = React.useState(JSON.parse(localStorage.getItem("blogData")) || {newBlog})
+
+    
+
+    
+    
+
+    function changeContent(event) {
+        const { value } = event.target
+        setBlog(prevBlog => prevBlog.map(blogItem => {
+            return {...blogItem, content: value}
+        }))
+
+        //console.log(value)
+        
+        
+        
+    }
+
+    function findBlog() {
+        return blog.find(note => {
+            return note
+        }) 
+    }
+
+
+    function post() {
+        const postedBlog = blog.map(prevBlog => prevBlog);
+      
+        const blogContent = postedBlog[0].content
+        
+        resultDiv.textContent = blogContent
+        
+        
+      }
+
+
+    
+     
+
     return (
         <div>
-            <h1>Profile Page...</h1>
-            <p>Write your code in this div</p>
+            <Blog 
+                createBlog={newBlog}
+                changeContent={changeContent}
+                currentBlog={findBlog()}
+                post={post}
+            />
         </div>
     )
 }
