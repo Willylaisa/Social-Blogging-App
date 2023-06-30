db8dd38889479042a21d8df6c4195355adc8dbc4
Brian <134489011+Brikita@users.noreply.github.com>
2023-06-30 21:08:05 +0300

    commit 7 resolve possible conflicts

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./blog.css"
+
+export default function Blog() {
+    return (
+        <div>
+            <h1>Blog Page...</h1>
+            <p>Write your code in this div</p>
+        </div>
+    )
+}
