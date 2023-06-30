22de0e7c0c14bd84518e88b3115c32d2370ff226
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2023-06-30 15:16:44 +0300

    adding functionality& reusabilityusing props&state

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
-import "./profile.css"
+import React from "react"
+import Details from "../../components/Details"
+import Header from "../../components/Header"
 
 export default function Profile() {
-    return (
-        <div>
-            <h1>Profile Page...</h1>
-            <p>Write your code in this div</p>
-        </div>
+    // STATES
+    const [profile, setProfile] = React.useState(
+        JSON.parse(localStorage.getItem("profile")) ||
+        {profileName: "", description: ""}
+    )  
+    const [editing, setEditing] = React.useState(false);
+
+    // STORING TO LOCAL STORAGE
+    React.useEffect(function() {
+        localStorage.setItem("profile", JSON.stringify(profile))
+    }, [profile])
+    
+    
+    // FUNCTIONS TO HANDLE EVENTS
+    function handleChange(event) {              
+        return (
+            setProfile(prevProfile => {
+                return {
+                    ...prevProfile,
+                    [event.target.name]: event.target.value
+                }
+            })
+        )
+    }           
+            
+    
+    // CLICK FUNCTION
+    function editProfile() {
+        setEditing(prevEditing => !prevEditing)
+    }
+        
+        
+
+        return (
+            <div>
+        <Header />
+        <Details 
+        nameInput = {
+            editing ? (
+                <h2 className="profile-name">{profile.profileName}</h2>
+            ) : (
+                <input 
+                    className="profile--name"
+                    name="profileName" 
+                    type="text" 
+                    placeholder="Your Name" 
+                    value={profile.profileName} 
+                    onChange={handleChange}
+                ></input>
+            )
+        }
+        descriptionInput = {
+            editing ? (
+                <p className="profile--paragraph">{profile.description}</p>
+            ) : (
+                <input
+                    className="profile--description" 
+                    name="description"
+                    type="text" 
+                    placeholder="Write your bio" 
+                    value={profile.description} 
+                    onChange={handleChange}
+                ></input>
+            )
+        }
+        handleClick={editProfile}
+        />
+    </div>
     )
-}
+    }
